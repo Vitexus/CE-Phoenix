a65530e7e6beb81e91d4bad6373dffefb044901d
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-06-13 04:08:15 +0200

    Czech localization update

--- includes/languages/czech/modules/content/index/cm_i_customer_greeting.php
+++ includes/languages/czech/modules/content/index/cm_i_customer_greeting.php
@@ -13,5 +13,5 @@
   const MODULE_CONTENT_CUSTOMER_GREETING_TITLE       = 'Pozdrav zákazníka';
   const MODULE_CONTENT_CUSTOMER_GREETING_DESCRIPTION = 'Zobrazí modul "Přivítání zákazníka" na hlavní stránce.';
 
-  const MODULE_CONTENT_CUSTOMER_GREETING_PERSONAL    = '<h4 class="alert-heading">Vítejte zpět %s!</h4> Would you like to see which <a class="alert-link" href="%s">new products</a> are available to purchase?';
-  const MODULE_CONTENT_CUSTOMER_GREETING_GUEST       = '<h4 class="alert-heading">Vítej hoste!</h4> Would you like to <a class="alert-link" href="%s">log yourself in</a> or would you prefer to <a class="alert-link" href="%s">create a profile</a> for easy checkout';
+  const MODULE_CONTENT_CUSTOMER_GREETING_PERSONAL    = '<h4 class="alert-heading">Vítejte zpět %s!</h4> Chcete vidět které <a class="alert-link" href="%s">nové produkty</a> je možno objednat?';
+  const MODULE_CONTENT_CUSTOMER_GREETING_GUEST       = '<h4 class="alert-heading">Vítej hoste!</h4> Chcete se <a class="alert-link" href="%s">přihlásit </a> nebo chcete <a class="alert-link" href="%s">vytvořit účet</a> pro snadnou objednávku';
---
+++ includes/languages/czech/modules/pi/product_info/pi_gallery.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  const PI_GALLERY_TITLE       = 'Galerie';
+  const PI_GALLERY_DESCRIPTION = 'Zobrazuje obrázky produktu na stránce informací produktu.<div class="alert alert-info">Toto je podmodul pro omezení při použití v &pi; systému.</div>';
+  
+  const PI_GALLERY_ALBUM_NAME = 'Album pro %s';
+  const PI_GALLERY_ALBUM_CLOSE = 'Zavři';
+  
+  
